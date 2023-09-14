fc1bd24b328782588a461d3a529a20b8a10c6e02
Bard of Code <harnettmusic@gmail.com>
2023-09-14 14:26:51 -0230

    Added react routes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>Saxophone practice app</h1>
+      <p>Exercise image will go here</p>
+      <p>exercise description will go here</p>
     </div>
   );
 }
