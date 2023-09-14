cbe3ec3808e67501e464180795b4fd16ec797065
Bard of Code <harnettmusic@gmail.com>
2023-09-14 14:26:57 -0230

    Added react routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
 
-function App() {
+function Home() {
   return (
-    <div className="App">
-      <h1>Saxophone practice app</h1>
-      <p>Exercise image will go here</p>
-      <p>exercise description will go here</p>
+    <div>
+      <h1>Home & sign in page</h1>
     </div>
-  );
-}
+  )
+};
 
-export default App;
+export function Student() {
+  return (
+    <div>
+      <h1>Student practice page</h1>
+    </div>
+  )
+};
+
+export function Teacher() {
+  return (
+    <div>
+      <h1>Teacher assignment page</h1>
+    </div>
+  )
+};
+
+
+export function App() {
+  return <Home />;
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { App, Student, Teacher } from './App';
 import reportWebVitals from './reportWebVitals';
+import { 
+  BrowserRouter, 
+  Routes, 
+  Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<App />} />
+    <Route path="/student" element={<Student />} />
+    <Route path="/teacher" element={<Teacher />} />
+  </Routes>
+    
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
